1ce04499539e0ee060a5faf93c229496ff8625cd
aliculPix4D <aleksandar.licul_ext@pix4d.com>
2022-01-12 11:16:05 +0100

    add cmd args parsing to gaston

--- gaston/main.c
+++ gaston/main.c
@@ -2,9 +2,29 @@
 #include <stdio.h>
 #include "morse/morse.h"
 
+void usage() {
+    printf("usage: gaston <command> <options>\n");
+    printf("\ncommands:\n");
+	printf("morse    [-elrs] [-d device] [-w speed] [-c speed] [-f frequency] [string ...]\n");
+}
+
 int main(int argc, char *argv[])
-{	
-	printf("gaston_change\n");
-	morseExec(argc, argv);
+{
+	// avoid using  platform specific (only POSIX) getopt() to 
+	// ease porting to Windows
+	if( argc == 1) {
+		usage();
+		exit(0);
+	}
+	char *cmd = argv[1];
+	switch (*cmd) {
+	case 'm':
+		argv[1] = "gaston: morse";
+		morseExec(argc-1, argv + 1);
+		break;
+	default:
+		usage();
+		exit(0);
+	}
 	exit(0);
 }
